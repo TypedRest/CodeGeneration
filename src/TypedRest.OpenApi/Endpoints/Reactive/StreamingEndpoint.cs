38845c61266c194867fb49e3ed7f77c8af10e2a8
Bastian Eicher <bastian@eicher.net>
2019-10-27 19:49:24 +0100

    Added missing schema property for StreamingEndpoint

--- src/TypedRest.OpenApi/Endpoints/Reactive/StreamingEndpoint.cs
+++ src/TypedRest.OpenApi/Endpoints/Reactive/StreamingEndpoint.cs
@@ -1,5 +1,6 @@
 using Microsoft.OpenApi;
 using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
 using Microsoft.OpenApi.Writers;
 
 namespace TypedRest.OpenApi.Endpoints.Reactive
@@ -10,6 +11,11 @@ namespace TypedRest.OpenApi.Endpoints.Reactive
     public class StreamingEndpoint : Endpoint
     {
         public override string Type => "streaming";
+
+        /// <summary>
+        /// Schema describing the representation of individual elements in the strean.
+        /// </summary>
+        public OpenApiSchema Schema { get; set; }
 
         /// <summary>
         /// The character sequence used to detect that a new element starts in an HTTP stream.
@@ -25,14 +31,23 @@ namespace TypedRest.OpenApi.Endpoints.Reactive
         {
             base.Parse(data, parser);
 
+            Schema = data.GetSchema("schema");
             Separator = data.GetString("separator");
             BufferSize = data.GetInt("buffer-size");
+        }
+
+        public override void ResolveReferences(OpenApiComponents components)
+        {
+            base.ResolveReferences(components);
+
+            Schema = Schema?.Resolve(components);
         }
 
         protected override void WriteBody(IOpenApiWriter writer, OpenApiSpecVersion specVersion)
         {
             base.WriteBody(writer, specVersion);
 
+            writer.WriteOptionalObject("schema", Schema, specVersion);
             writer.WriteProperty("separator", Separator);
             writer.WriteProperty("buffer-size", BufferSize);
         }
