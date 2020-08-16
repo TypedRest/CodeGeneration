27b7b9cb2a3073d53066283b3e0cf4c1c64efda1
Bastian Eicher <bastian@eicher.net>
2020-08-16 14:51:23 +0200

    Added reference to NanoByte.CodeGeneration API documentation

--- doc/build.ps1
+++ doc/build.ps1
@@ -2,6 +2,10 @@
 $ErrorActionPreference = "Stop"
 pushd $PSScriptRoot
 
+echo "Downloading references to other documentation..."
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]'Tls11,Tls12'
+Invoke-WebRequest https://code-generation.nano-byte.net/nanobyte-code-generation.tag -OutFile nanobyte-code-generation.tag
+
 if (Test-Path ..\artifacts\Documentation) {rm -Recurse -Force ..\artifacts\Documentation}
 mkdir ..\artifacts\Documentation | Out-Null
 
--- doc/build.sh
+++ doc/build.sh
@@ -2,6 +2,9 @@
 set -e
 cd `dirname $0`
 
+echo "Downloading references to other documentation..."
+curl -sS -o nanobyte-code-generation.tag https://code-generation.nano-byte.net/nanobyte-code-generation.tag
+
 rm -rf ../artifacts/Documentation
 mkdir -p ../artifacts/Documentation
 
