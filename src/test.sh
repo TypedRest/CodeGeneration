8ae8f29cc0f370dcd0af1d4a65d91a3c0ecf118a
Bastian Eicher <bastian@eicher.net>
2026-08-10 15:03:00 +0200

    Added TypeScript support to CLI

--- src/test.sh
+++ src/test.sh
@@ -11,3 +11,19 @@ fi
 
 # Unit tests
 $dotnet test --no-build --logger trx --configuration Release UnitTests/UnitTests.csproj
+
+# TypeScript smoke test
+if command -v npm > /dev/null 2> /dev/null; then
+    cli="../artifacts/Release/net10.0/TypedRest.CodeGeneration.Cli.dll"
+
+    # Generate clients
+    rm -rf SmokeTest.TypeScript/generated
+    $dotnet "$cli" generate -l typescript -f UnitTests/sample-v3.yml -o SmokeTest.TypeScript/generated/sample -s Sample --generate-dtos
+    $dotnet "$cli" generate -l typescript -f SmokeTest/nested.yml -o SmokeTest.TypeScript/generated/nested -s Nested --generate-dtos
+
+    cd SmokeTest.TypeScript
+    npm install
+    npm run check
+else
+    echo "Skipping TypeScript smoke test: npm not found"
+fi
