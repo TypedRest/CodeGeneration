5a663698df2d1d43d61f43947b53c667b9048311
Bastian Eicher <bastian@eicher.net>
2026-08-11 01:10:51 +0200

    Use 0install run fetch NPM on-demand

--- src/test.ps1
+++ src/test.ps1
@@ -1,34 +1,34 @@
 $ErrorActionPreference = "Stop"
 pushd $PSScriptRoot
 
+$zeroInstall = "$PSScriptRoot\..\0install.ps1"
+
 function Run-DotNet {
-    ..\0install.ps1 run --batch --version 10.0..!10.1 https://apps.0install.net/dotnet/sdk.xml @args
+    & $zeroInstall run --batch --version 10.0..!10.1 https://apps.0install.net/dotnet/sdk.xml @args
     if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
 }
 
 function Run-Npm {
-    npm @args
+    if (Get-Command npm -ErrorAction SilentlyContinue) {
+        npm @args
+    } else {
+        & $zeroInstall run --batch --command=npm https://apps.0install.net/javascript/node.xml @args
+    }
     if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
 }
 
 # Unit tests
 Run-DotNet test --no-build --logger trx --configuration Release UnitTests\UnitTests.csproj
 
+$cli = "..\artifacts\Release\net10.0\TypedRest.CodeGeneration.Cli.dll"
+
 # TypeScript smoke test
-if (Get-Command npm -ErrorAction SilentlyContinue) {
-    $cli = "..\artifacts\Release\net10.0\TypedRest.CodeGeneration.Cli.dll"
-
-    # Generate clients
-    if (Test-Path SmokeTest.TypeScript\generated) {Remove-Item SmokeTest.TypeScript\generated -Recurse -Force}
-    Run-DotNet $cli generate -l typescript -f UnitTests\sample-v3.yml -o SmokeTest.TypeScript\generated\sample -s Sample --generate-dtos
-    Run-DotNet $cli generate -l typescript -f UnitTests\sample-nested.yml -o SmokeTest.TypeScript\generated\nested-sample -s NestedSample --generate-dtos
-
-    pushd SmokeTest.TypeScript
-    Run-Npm ci
-    Run-Npm run check
-    popd
-} else {
-    Write-Host "Skipping TypeScript smoke test: npm not found"
-}
+if (Test-Path SmokeTest.TypeScript\generated) {Remove-Item SmokeTest.TypeScript\generated -Recurse -Force}
+Run-DotNet $cli generate -l typescript -f UnitTests\sample-v3.yml -o SmokeTest.TypeScript\generated\sample -s Sample --generate-dtos
+Run-DotNet $cli generate -l typescript -f UnitTests\sample-nested.yml -o SmokeTest.TypeScript\generated\nested-sample -s NestedSample --generate-dtos
+pushd SmokeTest.TypeScript
+Run-Npm ci
+Run-Npm run check
+popd
 
 popd
--- src/test.sh
+++ src/test.sh
@@ -2,28 +2,33 @@
 set -e
 cd `dirname $0`
 
+zeroinstall="`cd .. && pwd`/0install.sh"
+
 # Find dotnet
 if command -v dotnet > /dev/null 2> /dev/null; then
     dotnet="dotnet"
 else
-    dotnet="../0install.sh run --version 10.0..!10.1 https://apps.0install.net/dotnet/sdk.xml"
+    dotnet="$zeroinstall run --version 10.0..!10.1 https://apps.0install.net/dotnet/sdk.xml"
+fi
+
+# Find npm
+if command -v npm > /dev/null 2> /dev/null; then
+    npm="npm"
+else
+    npm="$zeroinstall run --command=npm https://apps.0install.net/javascript/node.xml"
 fi
 
 # Unit tests
 $dotnet test --no-build --logger trx --configuration Release UnitTests/UnitTests.csproj
 
+cli="../artifacts/Release/net10.0/TypedRest.CodeGeneration.Cli.dll"
+
 # TypeScript smoke test
-if command -v npm > /dev/null 2> /dev/null; then
-    cli="../artifacts/Release/net10.0/TypedRest.CodeGeneration.Cli.dll"
-
-    # Generate clients
-    rm -rf SmokeTest.TypeScript/generated
-    $dotnet "$cli" generate -l typescript -f UnitTests/sample-v3.yml -o SmokeTest.TypeScript/generated/sample -s Sample --generate-dtos
-    $dotnet "$cli" generate -l typescript -f UnitTests/sample-nested.yml -o SmokeTest.TypeScript/generated/nested-sample -s NestedSample --generate-dtos
-
+rm -rf SmokeTest.TypeScript/generated
+$dotnet "$cli" generate -l typescript -f UnitTests/sample-v3.yml -o SmokeTest.TypeScript/generated/sample -s Sample --generate-dtos
+$dotnet "$cli" generate -l typescript -f UnitTests/sample-nested.yml -o SmokeTest.TypeScript/generated/nested-sample -s NestedSample --generate-dtos
+(
     cd SmokeTest.TypeScript
-    npm ci
-    npm run check
-else
-    echo "Skipping TypeScript smoke test: npm not found"
-fi
+    $npm ci
+    $npm run check
+)
